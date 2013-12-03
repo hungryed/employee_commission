73b4c901a3044299faf07f558cb90d5431cfc3f7
Joe Sutton <hungryed6@gmail.com>
2013-12-03 15:41:52 -0500

    sales reading into SalesReader

--- sales_reader.rb
+++ sales_reader.rb
@@ -1,7 +1,20 @@
 class SaleReader
+  attr_accessor :sales
 
  def initialize(filename)
   @filename = filename
+  @sales = []
+  read_sales
+ end
+
+ def read_sales
+  CSV.foreach(@filename, headers:true, header_converters: :symbol) do |row|
+    sale = {
+      employee: row[:last_name],
+      sale_amount: row[:gross_sale_value]
+    }
+    @sales << sale
+  end
  end
 
 end
